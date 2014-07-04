ea759aa8ceb0f0751fd9122eb06153b017622aa6
Eduardogit <itenriquez_isc@hotmail.com>
2014-07-03 22:48:56 -0500

    Second Update V2 using Wget

--- pythonCrawler/glosbeCrawler.py
+++ pythonCrawler/glosbeCrawler.py
@@ -1,1 +1,16 @@
-print("\n\n\n\n\t\t:::::GlosbeDev::::\n\n\n\n")
+import os
+#========================================#
+#os libreria para acceder a comandos de  #
+#la terminal local                       #
+#========================================#
+try:
+	print("\n\n\n\n\t\t:::::GlosbeDev::::\n\n\n\n")
+	page = raw_input("ingresa una direccion url\n\n>:")
+	#============================================#
+	#raw_input() funciona para entrada de strings#
+	#input() funciona para entrada de enteros    #
+	#wget mas -r mas -p descarga una pagina web  #
+	#============================================#
+	os.system('wget  -r -p http://www.'+page)
+except:
+	print ("Error en algun parametro"+e)
